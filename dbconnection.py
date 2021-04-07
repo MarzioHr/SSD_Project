e10dac67e6052a2293c39ca16b6ee7915056e5c8
MarzioHr <marzioh@protonmail.com>
2021-04-07 13:55:47 +0000

    Removed Testing Comment inside the DB Connection Module

--- dbconnection.py
+++ dbconnection.py
@@ -1,7 +1,9 @@
 from cryptography.fernet import Fernet # lib to decrypt Postgresql credentials from binary file
 import psycopg2 # Postgresql connector library
 
-# Try retrieving the Fernet encryption key from bin file
+# Try retriev
+# 
+# ing the Fernet encryption key from bin file
 try:
   loginFRetrieve = open("config/key.bin", "rb")
   retrievedKey = loginFRetrieve.read()
@@ -23,10 +25,10 @@ credential = cipher.decrypt(retrievedCred)
 credential = credential.decode('utf-8')
 splitCreds = credential.split(":")
 
+
 # Try connecting to Postgresql DB with decrypted credentials
 try:
   conn = psycopg2.connect(host=splitCreds[0],dbname=splitCreds[1],user=splitCreds[2],password=splitCreds[3])
 
 except:
   print("Postgresql connection failed.")
-#Test
