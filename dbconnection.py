d7dcf578a000edf73fdaaed13448ecd2df6fed9b
MarzioHr <marzioh@protonmail.com>
2021-04-07 13:57:46 +0000

    Changed Variables to Snake Case

--- dbconnection.py
+++ dbconnection.py
@@ -1,12 +1,10 @@
 from cryptography.fernet import Fernet # lib to decrypt Postgresql credentials from binary file
 import psycopg2 # Postgresql connector library
 
-# Try retriev
-# 
-# ing the Fernet encryption key from bin file
+# Try retrieving the Fernet encryption key from bin file
 try:
   loginFRetrieve = open("config/key.bin", "rb")
-  retrievedKey = loginFRetrieve.read()
+  retrieved_key = loginFRetrieve.read()
   loginFRetrieve.close()
 except:
   print("Error retrieving key.")
@@ -14,21 +12,21 @@ except:
 # Try retrieving the Postgresql credentials from bin file
 try:
   loginFRetrieve = open("config/credentials.bin", "rb")
-  retrievedCred = loginFRetrieve.read()
+  retrieved_cred = loginFRetrieve.read()
   loginFRetrieve.close()
 except:
   print("Error retrieving credentials.")
   
 # Decrypt the retrieved Postgresql creds and split into list
-cipher = Fernet(retrievedKey)
-credential = cipher.decrypt(retrievedCred)
+cipher = Fernet(retrieved_key)
+credential = cipher.decrypt(retrieved_cred)
 credential = credential.decode('utf-8')
-splitCreds = credential.split(":")
+split_creds = credential.split(":")
 
 
 # Try connecting to Postgresql DB with decrypted credentials
 try:
-  conn = psycopg2.connect(host=splitCreds[0],dbname=splitCreds[1],user=splitCreds[2],password=splitCreds[3])
+  conn = psycopg2.connect(host=split_creds[0],dbname=split_creds[1],user=split_creds[2],password=split_creds[3])
 
 except:
   print("Postgresql connection failed.")
